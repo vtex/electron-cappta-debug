b19d7f93f3499b6f93a6b83f3f4945b0120d8900
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-05-09 11:45:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# electron-quick-start
+
+**Clone and run for a quick way to see Electron in action.**
+
+This is a minimal Electron application based on the [Quick Start Guide](http://electron.atom.io/docs/tutorial/quick-start) within the Electron documentation.
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/vtex/electron-cappta-debug
+# Go into the repository
+cd electron-cappta-debug
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
+
+## Resources for Learning Electron
+
+- [electron.atom.io/docs](http://electron.atom.io/docs) - all of Electron's documentation
+- [electron.atom.io/community/#boilerplates](http://electron.atom.io/community/#boilerplates) - sample starter apps created by the community
+- [electron/electron-quick-start](https://github.com/electron/electron-quick-start) - a very basic starter Electron app
+- [electron/simple-samples](https://github.com/electron/simple-samples) - small applications with ideas for taking them further
+- [electron/electron-api-demos](https://github.com/electron/electron-api-demos) - an Electron app that teaches you how to use Electron
+- [hokein/electron-sample-apps](https://github.com/hokein/electron-sample-apps) - small demo apps for the various Electron APIs
---
+++ main.js
@@ -0,0 +1,75 @@
+const electron = require('electron')
+// Module to control application life.
+const app = electron.app
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: 'Cappta Testing Checkout',
+    webPreferences: {
+      allowRunningInsecureContent: true,
+    },
+  })
+
+  // allow cappta https error
+  mainWindow.webContents.on('certificate-error', (event, url, error, certificate, callback) => {
+    if (url.indexOf('cappta.api.s3.amazonaws.com') !== -1) {
+      // verification logic
+      event.preventDefault()
+      callback(true)
+    } else {
+      callback(false)
+    }
+  })
+
+  mainWindow.maximize()
+
+  // Load cappta url with example params
+  mainWindow.loadURL('https://instore.vtexcommercestable.com.br/checkout/instore-cappta?acquirerProtocol=cappta&action=payment&installmentType=2&installments=1&paymentType=credit&amount=100&installmentsInterestRate=0&scheme=instore&autoConfirm=true&cnpj=42724382000146&authKey=DB780A0AB89845D283FF8C779638B54D&authPassword=vtex&administrativePassword=cappta')
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
